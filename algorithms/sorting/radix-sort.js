0f46d630a2ef20b83f16da9c480b60ed67ac427d
Satish Maurya <maurya777@gmail.com>
2022-07-12 17:34:59 +0000

    Added radix sort

---
+++ algorithms/sorting/radix-sort.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+function getDigit(num, place) {
+  return Math.floor(Math.abs(num) / 10 ** place) % 10;
+}
+
+function radixSort(arr, place = 0) {
+  // const acc = [];
+  // if (!arr.length) return arr;
+  // for (let i = 0; i < arr.length; i++) {
+  //   const digit = getDigit(arr[i], place);
+  //   acc[digit] = acc[digit] || [];
+  //   acc[digit].push(arr[i]);
+  // }
+  // return acc.length === 1 ? acc[0] : radixSort([].concat(...acc), place + 1);
+  if (!arr.length) return arr;
+  const acc = arr.reduce((acc, num) => {
+    const digit = getDigit(num, place);
+    acc[digit] = acc[digit] || [];
+    acc[digit].push(num);
+    return acc;
+  }, []);
+  return acc.length === 1 ? acc[0] : radixSort([].concat(...acc), place + 1);
+}
+
+if (require?.main === module) {
+  console.log(
+    radixSort(
+      Array(parseInt(Math.random() * 100, 10))
+        .fill()
+        .map((_) => parseInt(Math.random() * 100, 10))
+    )
+  );
+}
+
+module.exports = radixSort;
